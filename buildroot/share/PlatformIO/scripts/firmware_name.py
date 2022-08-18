6040c74c227569c8917bd5f01003972c9a793c73
junland <unlandj2012@gmail.com>
2022-08-18 13:40:12 -0500

    Hopefully fix the build.

--- buildroot/share/PlatformIO/scripts/firmware_name.py
+++ buildroot/share/PlatformIO/scripts/firmware_name.py
@@ -6,9 +6,9 @@ FW_NAME = os.getenv('FIRMWARE_NAME')
 
 # Check if FW_NAME is not empty
 if FW_NAME:
-    print("FIRMWARE_NAME set to %s" % FW_NAME)
+    print("FIRMWARE_NAME: %s" % FW_NAME)
     env.Replace(PROGNAME="%s" % FW_NAME)
 else:
     FW_NAME = datetime.now().strftime("firmware-%Y%m%d-%H%M%S")
     env.Replace(PROGNAME="%s" % FW_NAME)
-    print("FIRMWARE_NAME variable is empty, will set name to: %s" % FW_NAME)
+    print("FIRMWARE_NAME: %s" % FW_NAME)
